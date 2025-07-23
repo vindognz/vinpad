dd022a0495ffbabf3cf1d7f1f516e4a0cf0b893b
Vincent Rodley <vindognz@gmail.com>
2025-07-24 09:59:59 +1200

    added instructions for what to do when i get the hardware

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # vinpad
+
+stuff to do when i get the actual hardware:
+
+plug into pc via usb while holding the boot button
+drag the uf2 file onto the RPI-RP2 drive
+(should reboot and mount as CIRCUITPY)
+
+copy code.py and lib onto the root CIRCUITPY drive
+(should look like)
+CIRCUITPY/
+├── code.py
+└── lib/
+    ├── kmk/
+    └── adafruit_hid/
+
+open a text editor and hit all the buttons. it should type 1-6 (spinny thing should mess with volume)
--- code.py
+++ code.py
@@ -6,7 +6,7 @@ from kmk.modules.encoder import EncoderHandler
 
 keyboard = KMKKeyboard()
 
-# Define direct pins for 6 switches
+# Define pins for 6 buttons (direct input)
 keyboard.matrix = Scanner(
     [PinDirect(pin) for pin in (
         board.GP26,
@@ -18,21 +18,21 @@ keyboard.matrix = Scanner(
     )]
 )
 
-# Optional rotary encoder support
+# Rotary encoder setup (volume control)
 encoder_handler = EncoderHandler()
-encoder_handler.encoder_pins = [(board.GP2, board.GP1)]  # A, B
+encoder_handler.encoder_pins = [(board.GP2, board.GP1)]
 encoder_handler.map = [((KC.VOLD, KC.VOLU),)]
 keyboard.modules.append(encoder_handler)
 
-# Keymap: Ctrl+Shift+Alt+1 through 6
+# Keymap: just send digits 1-6
 keyboard.keymap = [
     [
-        KC.LCTRL(KC.LSHIFT(KC.LALT(KC._1))),  # Button 1
-        KC.LCTRL(KC.LSHIFT(KC.LALT(KC._2))),  # Button 2
-        KC.LCTRL(KC.LSHIFT(KC.LALT(KC._3))),  # Button 3
-        KC.LCTRL(KC.LSHIFT(KC.LALT(KC._4))),  # Button 4
-        KC.LCTRL(KC.LSHIFT(KC.LALT(KC._5))),  # Button 5
-        KC.LCTRL(KC.LSHIFT(KC.LALT(KC._6))),  # Button 6
+        KC._1,  # Button 1 sends '1'
+        KC._2,  # Button 2 sends '2'
+        KC._3,  # Button 3 sends '3'
+        KC._4,  # Button 4 sends '4'
+        KC._5,  # Button 5 sends '5'
+        KC._6,  # Button 6 sends '6'
     ]
 ]
 
